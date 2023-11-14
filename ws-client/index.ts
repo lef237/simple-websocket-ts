b21725fc9993ac58cce8413282177fe42a3b2f8e
lef237 <lef237k5@gmail.com>
2023-11-14 21:07:07 +0900

    Introduce WebSocket to server and client

---
+++ ws-client/index.ts
@@ -0,0 +1,40 @@
+import WebSocket from "ws";
+import readline from "readline";
+
+const url = "ws://localhost:8080";
+const ws = new WebSocket(url);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+ws.onopen = () => {
+  console.log("Connected to the server.");
+  promptInput();
+};
+
+function promptInput() {
+  rl.question("Enter message to send: ", (message) => {
+    if (message === "exit") { // 'exit'と入力した場合、プログラムを終了
+      rl.close();
+      ws.close();
+    } else {
+      ws.send(message);
+      promptInput();
+    }
+  });
+}
+
+ws.onmessage = (e) => {
+  console.log(`Received: ${e.data}`);
+};
+
+ws.onerror = (error) => {
+  console.error(`WebSocket Error: ${error}`);
+};
+
+ws.onclose = () => {
+  console.log("Disconnected from the server");
+  rl.close();
+};
---
+++ ws-server/index.ts
@@ -0,0 +1,17 @@
+import { Server } from "ws";
+
+const port = 8080; // サーバーのポート番号
+const server = new Server({ port });
+
+console.log(`WebSocket Server is running on port ${port}`);
+
+server.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(`Received message => ${message}`);
+    ws.send(`Hello, Client! Your message was: ${message}`); // クライアントに応答を送信
+  });
+
+  ws.on("close", () => {
+    console.log("Client has disconnected");
+  });
+});
