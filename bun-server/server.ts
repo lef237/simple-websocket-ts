16a5fbc7e0b064dcf80e5f7a55cb019fa9fad33f
lef237 <lef237k5@gmail.com>
2023-11-17 14:21:36 +0900

    Change indentation style of formatter from tab to space

--- bun-server/server.ts
+++ bun-server/server.ts
@@ -2,21 +2,21 @@ const port = 8080; // サーバーのポート番号
 console.log(`WebSocket Server is running on port ${port}`);
 
 Bun.serve({
-	fetch(req, server) {
-		// upgrade the request to a WebSocket
-		if (server.upgrade(req)) {
-			return; // do not return a Response
-		}
-		return new Response("Upgrade failed :(", { status: 500 });
-	},
-	port,
-	websocket: {
-		message(ws, message) {
-			console.log(`Received message => ${message}`);
-			ws.send(`Hello, Client! Your message was: ${message}`); // クライアントに応答を送信
-		},
-		close() {
-			console.log("Client has disconnected");
-		},
-	},
+  fetch(req, server) {
+    // upgrade the request to a WebSocket
+    if (server.upgrade(req)) {
+      return; // do not return a Response
+    }
+    return new Response("Upgrade failed :(", { status: 500 });
+  },
+  port,
+  websocket: {
+    message(ws, message) {
+      console.log(`Received message => ${message}`);
+      ws.send(`Hello, Client! Your message was: ${message}`); // クライアントに応答を送信
+    },
+    close() {
+      console.log("Client has disconnected");
+    },
+  },
 });
--- ws-client/client.ts
+++ ws-client/client.ts
@@ -5,37 +5,37 @@ const url = "ws://localhost:8080";
 const ws = new WebSocket(url);
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 ws.onopen = () => {
-	console.log("Connected to the server.");
-	promptInput();
+  console.log("Connected to the server.");
+  promptInput();
 };
 
 function promptInput() {
-	rl.question("Enter message to send: ", (message) => {
-		if (message === "exit") {
-			// 'exit'と入力した場合、プログラムを終了
-			rl.close();
-			ws.close();
-		} else {
-			ws.send(message);
-		}
-	});
+  rl.question("Enter message to send: ", (message) => {
+    if (message === "exit") {
+      // 'exit'と入力した場合、プログラムを終了
+      rl.close();
+      ws.close();
+    } else {
+      ws.send(message);
+    }
+  });
 }
 
 ws.onmessage = (e) => {
-	console.log(`Received: ${e.data}`);
-	promptInput();
+  console.log(`Received: ${e.data}`);
+  promptInput();
 };
 
 ws.onerror = (error) => {
-	console.error(`WebSocket Error: ${error}`);
+  console.error(`WebSocket Error: ${error}`);
 };
 
 ws.onclose = () => {
-	console.log("Disconnected from the server");
-	rl.close();
+  console.log("Disconnected from the server");
+  rl.close();
 };
