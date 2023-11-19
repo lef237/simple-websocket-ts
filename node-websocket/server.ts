0f21d96439a77f97d85b14b93501d3349100f7e6
lef237 <lef237k5@gmail.com>
2023-11-19 15:46:35 +0900

    Send message from server to each client

--- node-websocket/server.ts
+++ node-websocket/server.ts
@@ -1,17 +1,28 @@
-import { Server } from "ws";
+import { Server, WebSocket } from "ws";
 
 const port = 8080; // サーバーのポート番号
 const server = new Server({ port });
+const clients = new Set<WebSocket>(); // 接続中のクライアントを格納するSet
 
 console.log(`WebSocket Server is running on port ${port}`);
 
 server.on("connection", (ws) => {
+  console.log("Client has connected");
+  clients.add(ws); // クライアントを追加
+
   ws.on("message", (message) => {
     console.log(`Received message => ${message}`);
-    ws.send(`Hello, Client! Your message was: ${message}`); // クライアントに応答を送信
+    // すべてのクライアントにメッセージをブロードキャスト
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(`\nClient said: ${message}`);
+      }
+    });
   });
 
   ws.on("close", () => {
     console.log("Client has disconnected");
+    clients.delete(ws); // クライアントを削除
   });
 });
